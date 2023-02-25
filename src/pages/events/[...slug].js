c1b81a149b4a08b3e3c0aa36abe8bf398c9b863e
Taras.Kravets <taraskravets95@gmail.com>
2023-02-25 21:50:51 +0200

    feat(UI): added Filtered Events Page

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,14 +1,39 @@
+import { useRouter } from 'next/router';
 import { getFeaturedEvents } from 'dummy-data';
+import WarningBox from '@/components/UI/WarningBox';
+import EventsList from '@/components/events/EventsList';
 
 function FilteredEventPage() {
+	const router = useRouter();
+	const filterData = router.query.slug;
+
+	if (!filterData) {
+		return <p style={{ textAlign: 'center' }}>Loading...</p>;
+	}
+
+	const filteredYear = +filterData[0];
+	const filteredMonth = +filterData[1];
+
+	if (
+		isNaN(filteredYear) ||
+		isNaN(filteredMonth || filteredMonth < 1 || filteredMonth > 12)
+	) {
+		return <WarningBox message='Invalid filter. Please adjust your values!' />;
+	}
+
 	const filteredEvents = getFeaturedEvents({
-		year: numYear,
-		month: numMonth,
+		year: filteredYear,
+		month: filteredMonth,
 	});
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return <WarningBox message='No events found for the chosen filter' />;
+	}
+
 	return (
-		<div>
-			<h1>Filtered Events</h1>
-		</div>
+		<>
+			<EventsList items={filteredEvents} />
+		</>
 	);
 }
 export default FilteredEventPage;

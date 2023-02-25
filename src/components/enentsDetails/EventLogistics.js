cc52c572d15bfbf52449b7ede64e391479289f27
Taras.Kravets <taraskravets95@gmail.com>
2023-02-25 13:44:22 +0200

    feat(UI): added eventsDetails components and styles

---
+++ src/components/enentsDetails/EventLogistics.js
@@ -0,0 +1,32 @@
+import AddressIcon from '@/components/icons/AddressIcon';
+import DateIcon from '@/components/icons/DateIcon';
+import LogisticsItem from './LogisticsItem';
+
+import styles from '@/styles/components/enentsDetails/EventLogistics.module.css';
+
+function EventLogistics({ date, address, image, imageAlt }) {
+	const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
+	const addressText = address?.replace(', ', '\n');
+
+	return (
+		<section className={styles.logistics}>
+			<div className={styles.image}>
+				<img src={`/${image}`} alt={imageAlt} />
+			</div>
+			<ul className={styles.list}>
+				<LogisticsItem icon={DateIcon}>
+					<time>{humanReadableDate}</time>
+				</LogisticsItem>
+				<LogisticsItem icon={AddressIcon}>
+					<address>{addressText}</address>
+				</LogisticsItem>
+			</ul>
+		</section>
+	);
+}
+
+export default EventLogistics;
--- src/pages/events/[eventId].js
+++ src/pages/events/[eventId].js
@@ -1,11 +1,15 @@
 import { useRouter } from 'next/router';
+
 import { getEventById } from 'dummy-data';
 import WarningBox from '@/components/UI/WarningBox';
+import EventSummary from '@/components/enentsDetails/EventSummary';
+import EventContent from '@/components/enentsDetails/EventContent';
+import EventLogistics from '@/components/enentsDetails/EventLogistics';
 
 function EventDetailPage() {
 	const router = useRouter();
 
-	const eventId = router.query.eventsId;
+	const eventId = router?.query?.eventId;
 	const event = getEventById(eventId);
 
 	if (!event) {
@@ -13,9 +17,18 @@ function EventDetailPage() {
 	}
 
 	return (
-		<div>
-			<h1>Event Detail</h1>
-		</div>
+		<>
+			<EventSummary title={event?.title} />
+			<EventLogistics
+				date={event?.date}
+				address={event?.location}
+				image={event?.image}
+				imageAll={event?.title}
+			/>
+			<EventContent>
+				<p>{event?.description}</p>
+			</EventContent>
+		</>
 	);
 }
 

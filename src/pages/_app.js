bd7a8e09e01c60aa4c84ecb037fc4f65e7be6ffb
Taras.Kravets <taraskravets95@gmail.com>
2023-02-28 15:00:55 +0200

    feat(UI): Added Notification component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import Layout from '@/components/layout/Layout';
+
+import Notification from '@/components/UI/Notification';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
 	return (
 		<Layout>
 			<Component {...pageProps} />
+			<Notification title='test' message='this is a test.' start='pending' />
 		</Layout>
 	);
 }
